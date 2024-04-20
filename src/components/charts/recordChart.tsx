384c5e29479318847df543340ab115be3c559959
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-04-20 15:20:53 +0530

    fetched list of records from aws

--- src/components/charts/recordChart.tsx
+++ src/components/charts/recordChart.tsx
@@ -1,52 +1,26 @@
-import { Tooltip } from 'antd';
 import React from 'react';
-import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
-import styled from 'styled-components';
- 
-const RecordChart:React.FC = () => {
-    const recordTypeData = [
-        { name: 'A', count: 3 },
-        { name: 'AAAA', count: 2 },
-        { name: 'CNAME', count: 5 },
-        { name: 'MX', count: 2 },
-        { name: 'NS', count: 3 },
-        { name: 'PTR', count: 6 },
-        { name: 'SOA', count: 1 },
-        { name: 'SRV', count: 9 },
-        { name: 'TXT', count: 5 },
-        { name: 'DNSSEC', count: 7 },
-    ];
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-    return (
-        <RecordChartComponent>
-            <RecordChartHeading>
-                <h2>RECORD TYPE DISTRIBUTION</h2>
-            </RecordChartHeading>
-            <RecordBarChart>
-                <BarChart width={400} height={300} data={recordTypeData}>
-                    <CartesianGrid />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="count" fill="#82ca9d" />
-                </BarChart>
-            </RecordBarChart>            
-        </RecordChartComponent>
-    );
+interface Props {
+  chartData: { type: string; value: number }[];
+}
+
+const RecordChart: React.FC<Props> = ({ chartData }) => {
+  return (
+    <LineChart
+      width={800}
+      height={400}
+      data={chartData}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="type" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+    </LineChart>
+  );
 };
- 
+
 export default RecordChart;
-
-const RecordChartComponent = styled.div`
-    margin-top: 3%;
-    margin-left: 1%;
-`;
-
-const RecordChartHeading = styled.div`
-    font-size: 80%;
-`;
-
-const RecordBarChart = styled.div`
-    margin-top: 2%;
-`;
--- src/components/record/viewRecordTable.tsx
+++ src/components/record/viewRecordTable.tsx
@@ -1,46 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Input, Table } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import { SearchOutlined, EditOutlined, DeleteRowOutlined } from '@ant-design/icons';
+import { Button, Input, InputRef, Space, Table } from 'antd';
+import Highlighter from 'react-highlight-words';
 import styled from 'styled-components';
+import DomainChart from '../charts/domainChart.tsx';
+import DeleteDNSRecordModal from '../modals/deleteDNSRecordModal.tsx';
+import EditDNSRecordModal from '../modals/editDNSRecordModal.tsx';
 import AWS from 'aws-sdk';
-import { ColumnsType } from 'antd/es/table/interface';
+import { ColumnType, ColumnsType } from 'antd/es/table/interface';
+import RecordChart from '../charts/recordChart.tsx';
+
+type DataIndex = keyof DataType;
 
 interface DataType {
-  key: string;
+  key: React.Key;
   recordName: string;
   recordType: string;
-  routingPolicy?: string | undefined;
+  routingPolicy?: string;
+  differentiator?: string;
   alias: string;
   valueOrRouteTrafficTo: string[];
   ttlInSeconds: number;
-  healthCheckId?: string | undefined;
-  evaluateTargetHealth?: string | undefined;
-  recordId?: string | undefined;
+  healthCheckId?: string;
+  evaluateTargetHealth?: string;
+  recordId?: string;
 }
 
 const ViewRecordTable: React.FC = () => {
   const [searchText, setSearchText] = useState<string>('');
+  const [searchTextOfColumn, setSearchTextOfColumn] = useState<string>('');
+  const [searchedColumn, setSearchedColumn] = useState<string>('');
+  const searchInputOfColumn = useRef<InputRef>(null);
+  const [viewChart, setViewChart] = useState<boolean>(false);
+  const [isEditButtonClicked, setIsEditButtonClicked] = useState<boolean>(false);
+  const [isDeleteButtonClicked, setIsDeleteButtonClicked] = useState<boolean>(false);
   const [dnsRecords, setDnsRecords] = useState<DataType[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [chartData, setChartData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const YOUR_HOSTED_ZONE_ID = 'Z03475321WH1NH01XRPEQ';
 
   useEffect(() => {
-    const fetchDNSRecords = async () => {
+    AWS.config.update({
+      accessKeyId: 'AKIASRL7FVZFLJTGFBYT',
+      secretAccessKey: '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',
+      region: 'us-east-1'
+    });
+
+    const route53 = new AWS.Route53();
+
+    const viewDNSRecord = async () => {
+      const domain = 'dnsmanager.live'; // You might want to replace this
+      const params = {
+        HostedZoneId: YOUR_HOSTED_ZONE_ID,
+        StartRecordName: domain,
+      };
+
       try {
-        AWS.config.update({
-          accessKeyId: 'AKIASRL7FVZFLJTGFBYT',
-          secretAccessKey: 'NTPeGEsBvab72xrgEuEP1OLHxzL1VwQBAqpySk0Q',
-          region: 'us-east-1'
-      });
-      
-        const route53 = new AWS.Route53();
-      
-        const domain = 'dnsmanager.live'; 
-        const params = {
-          HostedZoneId: 'Z03475321WH1NH01XRPEQ',
-          StartRecordName: domain,
-        };
-      
         const response = await route53.listResourceRecordSets(params).promise();
-      
         const records = response.ResourceRecordSets.map((record: any, index: number) => ({
           key: index.toString(),
           recordName: record.Name,
@@ -53,114 +70,222 @@ const ViewRecordTable: React.FC = () => {
           evaluateTargetHealth: record.EvaluateTargetHealth || '-',
           recordId: record.ResourceRecordSetId || '-'
         }));
-      
+        
         setDnsRecords(records);
-        setLoading(false);
+        setLoading(false); // Data fetching completed
       } catch (error) {
         console.error('Error getting DNS records:', error.message);
-        setLoading(false);
+        setLoading(false); // Error occurred while fetching data
       }
     };
-    
-    fetchDNSRecords();
+
+    viewDNSRecord();
+
+    return () => {
+      // Cleanup code if needed
+    };
   }, []);
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
+  const handleDeleteRecord = () => {
+    setIsDeleteButtonClicked(true);
   };
 
+  const handleEditRecord = () => {
+    setIsEditButtonClicked(true);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchTextOfColumn('');
+  };
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: () => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchTextOfColumn(selectedKeys[0]);
+    setSearchedColumn(dataIndex.toString());
+    setSearchText('');
+  };
+  
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={searchInputOfColumn}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0] || ''}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => clearFilters && handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    render: (text: string) =>
+      searchedColumn === dataIndex.toString() ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchTextOfColumn]}
+          autoEscape
+          textToHighlight={text.toString()}
+        />
+      ) : (
+        text
+      ),
+  });  
+
   const filteredData = dnsRecords.filter(record =>
-    record.recordName.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.recordType.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.routingPolicy?.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.alias.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.valueOrRouteTrafficTo.join(',').toLowerCase().includes(searchText.toLowerCase()) ||
-    record.ttlInSeconds.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-    record.healthCheckId?.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.evaluateTargetHealth?.toLowerCase().includes(searchText.toLowerCase()) ||
-    record.recordId?.toLowerCase().includes(searchText.toLowerCase()) 
+    (record.recordName.toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.recordType.toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.routingPolicy?.toLowerCase().includes(searchText.toLowerCase()) ?? false) ||
+    (record.alias.toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.valueOrRouteTrafficTo.join(',').toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.ttlInSeconds.toString().toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.healthCheckId?.toLowerCase().includes(searchText.toLowerCase())) ||
+    (record.evaluateTargetHealth?.toLowerCase().includes(searchText.toLowerCase()) ?? false) ||
+    (record.recordId?.toLowerCase().includes(searchText.toLowerCase()))
   );
 
   const columns: ColumnsType<DataType> = [
     {
-      title: 'Record Name',
+      title: 'Name',
       dataIndex: 'recordName',
-      key: 'recordName'
+      key: 'recordName',
+      ...getColumnSearchProps('recordName'),
     },
     {
       title: 'Record Type',
       dataIndex: 'recordType',
-      key: 'recordType'
+      key: 'recordType',
+      ...getColumnSearchProps('recordType'),
     },
     {
       title: 'Routing Policy',
       dataIndex: 'routingPolicy',
-      key: 'routingPolicy'
+      key: 'routingPolicy',
+      ...getColumnSearchProps('routingPolicy'),
     },
     {
       title: 'Alias',
       dataIndex: 'alias',
-      key: 'alias'
+      key: 'alias',
+      ...getColumnSearchProps('alias'),
     },
     {
       title: 'Value/Route Traffic To',
       dataIndex: 'valueOrRouteTrafficTo',
       key: 'valueOrRouteTrafficTo',
-      render: (values: string[]) => (
-        <ul>
-          {values.map((value, index) => (
-            <li key={index}>{value}</li>
-          ))}
-        </ul>
-      )
+      ...getColumnSearchProps('valueOrRouteTrafficTo'),
     },
     {
       title: 'TTL (in seconds)',
       dataIndex: 'ttlInSeconds',
-      key: 'ttlInSeconds'
+      key: 'ttlInSeconds',
+      ...getColumnSearchProps('ttlInSeconds'),
     },
     {
       title: 'Health Check ID',
       dataIndex: 'healthCheckId',
-      key: 'healthCheckId'
+      key: 'healthCheckId',
+      ...getColumnSearchProps('healthCheckId'),
     },
     {
       title: 'Evaluate Target Health',
       dataIndex: 'evaluateTargetHealth',
-      key: 'evaluateTargetHealth'
+      key: 'evaluateTargetHealth',
+      ...getColumnSearchProps('evaluateTargetHealth'),
     },
     {
       title: 'Record ID',
       dataIndex: 'recordId',
-      key: 'recordId'
-    }
+      key: 'recordId',
+      ...getColumnSearchProps('recordId'),
+    },
+    {
+      title: 'Edit',
+      dataIndex: 'editRecord',
+      key: 'editRecord',
+      render: () => <Button type="link" onClick={handleEditRecord}><EditOutlined /></Button>,
+    },
+    {
+      title: 'Delete',
+      dataIndex: 'deleteRecord',
+      key: 'deleteRecord',
+      render: () => <Button type="link" onClick={handleDeleteRecord}><DeleteRowOutlined /></Button>,
+    },
   ];
+
+  const handleViewChart = () => {
+    setViewChart(true);
+
+    const dataForChart = dnsRecords.map(record => ({
+      type: record.recordType,
+      value: record.ttlInSeconds
+    }));
+    
+    setChartData(dataForChart);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
 
   return (
     <ViewTableOfRecord>
-      <GlobalSearchOfTable>
-        <ViewChartButton >View Chart</ViewChartButton>
-        <Input
-          placeholder="Search"
-          allowClear
-          onChange={handleSearchChange}
-          style={{ width: '12%', marginBottom: '1%', justifyContent: 'flex-end' }}
-        />
-      </GlobalSearchOfTable>
-      <ViewContentOfTable>
-        {loading ? (
-          <LoadingText>Loading...</LoadingText>
-        ) : dnsRecords.length > 0 ? (
-          <ContentOfTable
-            columns={columns}
-            dataSource={filteredData}
-            pagination={false}
-            onChange={() => {}} 
-          />
-        ) : (
-          <LoadingText>No DNS records found</LoadingText>
-        )}
-      </ViewContentOfTable>
+     {viewChart ? (
+        <>
+          <RecordChart chartData = {chartData} />
+          <Button onClick={() => setViewChart(false)}>Back to Table</Button>
+        </>
+      )  : isDeleteButtonClicked ? (
+        <DeleteDNSRecordModal isDeleteButtonClicked={isDeleteButtonClicked} onCancel={() => setIsDeleteButtonClicked(false)} />
+      ) : isEditButtonClicked ? (
+        <EditDNSRecordModal isEditButtonClicked={isEditButtonClicked} onCancel={() => setIsEditButtonClicked(false)} />
+      ) : (
+        <>
+          <GlobalSearchOfTable>
+            <ViewChartButton onClick={handleViewChart}>View Chart</ViewChartButton>
+            <Input
+              placeholder="Search"
+              allowClear
+              onChange={handleSearchChange}
+              style={{ width: '12%', marginBottom: '1%', justifyContent: 'flex-end' }}
+            />
+          </GlobalSearchOfTable>
+          <ViewContentOfTable>
+            {loading ? (
+              <LoadingText>Loading...</LoadingText>
+            ) : dnsRecords.length > 0 ? (
+              <ContentOfTable
+                columns={columns}
+                dataSource={filteredData}
+                pagination={false}
+                onChange={() => {}} // Dummy function to prevent warning
+              />
+            ) : (
+              <LoadingText>No DNS records found</LoadingText>
+            )}
+          </ViewContentOfTable>
+        </>
+      )}
     </ViewTableOfRecord>
   );
 };
